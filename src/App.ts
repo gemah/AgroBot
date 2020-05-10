72f28b84fe20ca0f692b590f7ca2d400e93b69b1
jamai <jamaine3@live.de>
2020-05-10 23:43:02 +0200

    added a starting message

--- src/App.ts
+++ src/App.ts
@@ -6,6 +6,7 @@ import * as test from './Scenes/ConvertTemperaturesWizard'
 const stage = new Stage([test.TemperatureWizard]);
 
 const bot = new Telegraf("1135406683:AAEweez7_rB2iNuS5zucL_mpwHWnYMOUpVU");//process.env.TOKEN);
+console.log("Starting Bot");
 bot.use(session());
 bot.use(stage.middleware());
 
