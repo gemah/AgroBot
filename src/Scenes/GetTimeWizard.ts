8b55470dff75170a77bdd3af294de1b4a2eba061
gemah <hianneiva@gmail.com>
2020-06-13 13:00:48 -0300

    Formatting
    Formatting correction

--- src/Scenes/GetTimeWizard.ts
+++ src/Scenes/GetTimeWizard.ts
@@ -2,15 +2,16 @@
 import {Markup} from 'telegraf'
 import * as time from 'countries-and-timezones'
 import { CallbackButton } from 'telegraf/typings/markup';
+
 const WizardScene = require("telegraf/scenes/wizard");
+
 export const GetTimeWizard = new WizardScene(
     "get_time",
     ctx => {
       ctx.reply("Please Input the country you would like to have the current time from");
       return ctx.wizard.next();
     },
-    ctx => {
-        
+    ctx => {    
         if(!ctx.message){     
             ctx.reply("There was an error please try again");
             ctx.wizard.back();  // Set the listener to the previous function
@@ -55,17 +56,17 @@ export const GetTimeWizard = new WizardScene(
       
         ctx.reply(`The country ${ctx.message.text} has ${timezoneNames.length} tim6ezones\nPlease select the one you wish to use or just pick the Default one:`,
         Markup.inlineKeyboard(buttons/*buttons*/).extra());
+        
         return ctx.wizard.next();
     },
     ctx => {
-        
         const timeOffset = new Date().getTimezoneOffset() * 60000;
-        const index=Number(ctx.callbackQuery.data.replace("tz:",""));
+        const index = Number(ctx.callbackQuery.data.replace("tz:",""));
         const timeData = time.getTimezone(ctx.wizard.state.timezoneNames[index]);
         const offsetUTC:number = timeData["utcOffset"];
         const offsetDST:number = timeData["dstOffset"];
         const UTCTime = new Date(Date.now() + timeOffset + (offsetUTC)*60000);
-        const UTCTimeString =  `${String(UTCTime.getHours()).padStart(2,"0")}:${String(UTCTime.getMinutes()).padStart(2,"0")}:${String(UTCTime.getSeconds()).padStart(2,"0")}`;
+        const UTCTimeString = `${String(UTCTime.getHours()).padStart(2,"0")}:${String(UTCTime.getMinutes()).padStart(2,"0")}:${String(UTCTime.getSeconds()).padStart(2,"0")}`;
 
         if(offsetUTC == offsetDST){         
             ctx.reply(`The current time in ${ctx.wizard.state.timezoneNames[index]} is:\n${UTCTimeString}`);           
@@ -73,11 +74,12 @@ export const GetTimeWizard = new WizardScene(
 
         else{
             const DSTTime = new Date(Date.now() + timeOffset + (offsetDST * 60000));
-            const DSTTimeString =  `${String(DSTTime.getHours()).padStart(2,"0")}:${String(DSTTime.getMinutes()).padStart(2,"0")}:${String(DSTTime.getSeconds()).padStart(2,"0")}`;      
+            const DSTTimeString = `${String(DSTTime.getHours()).padStart(2,"0")}:${String(DSTTime.getMinutes()).padStart(2,"0")}:${String(DSTTime.getSeconds()).padStart(2,"0")}`;      
             ctx.reply(`The current time in ${ctx.wizard.state.timezoneNames[index].replace("_","\\_")} is:\n*${UTCTimeString}* if it isnt Daylight savings time\\.\n*${DSTTimeString}* if it is Daylight savings Time`,{parse_mode: 'markdownv2'});
         }
 
         console.log(timeData);
+        
         return ctx.scene.leave();
     }
   );
